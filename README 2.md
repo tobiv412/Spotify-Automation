a1be523f72cb6e907f57bd8b7404d1a90fedd178
tobiv412 <victor.tobi@cloudreach.com>
2022-01-05 18:54:43 +0000

    init

---
+++ README 2.md
@@ -0,0 +1,5 @@
+# Create a Spotify Playlist with Terraform
+
+Create a playlist on Spotify by writing it as a Terraform configuration.
+
+Follow along with the [tutorial](https://learn.hashicorp.com/tutorials/terraform/spotify-playlist) HashiCorp Learn.
---
+++ main.tf
@@ -0,0 +1,33 @@
+terraform {
+  required_providers {
+    spotify = {
+      version = "~> 0.2.6"
+      source  = "conradludgate/spotify"
+    }
+  }
+}
+
+provider "spotify" {
+  api_key = var.spotify_api_key
+}
+
+data "spotify_search_track" "by_artist" {
+  artist = "Summer Walker"
+  #album = "Still Over it"
+  #  name  = "Early Morning Breeze"
+}
+
+
+resource "spotify_playlist" "playlist" {
+  name        = "slow_night_vibes"
+  description = "This playlist was created by Terraform"
+  public      = true
+
+  tracks = [
+    data.spotify_search_track.by_artist.tracks[0].id,
+    data.spotify_search_track.by_artist.tracks[1].id,
+    data.spotify_search_track.by_artist.tracks[2].id,
+    data.spotify_search_track.by_artist.tracks[3].id,
+    data.spotify_search_track.by_artist.tracks[4].id,
+  ]
+}
